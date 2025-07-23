304fd41239df20f75a3d8fe37a78d340a25deaaf
Rawenvoys <lukwis95@gmail.com>
2025-07-23 14:33:22 +0200

    Update Roles.cs

--- src/Services/UserService/Domain/Enums/Roles.cs
+++ src/Services/UserService/Domain/Enums/Roles.cs
@@ -4,8 +4,8 @@ namespace Officely.UserService.Domain.Enums;
 
 public sealed class Roles : SmartFlagEnum<Roles, int>
 {
-    public static readonly Roles Customer = new(nameof(Customer), 1);
-    public static readonly Roles Admin = new(nameof(Admin), 2);
+    public static readonly Roles Admin = new(nameof(Admin), 1);
+    public static readonly Roles Client = new(nameof(Client), 2);
 
     private Roles(string name, int value) : base(name, value)
     {
